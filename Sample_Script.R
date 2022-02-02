cd3de0d1f03a891ab77d81c99dd7ecb2656ddf0e
kre8tivz <98755912+kre8tivz@users.noreply.github.com>
2022-02-02 16:47:44 +0100

    Merged Csv (Jan, Apr, Jul, Oct)

---
+++ Sample_Script.R
@@ -0,0 +1,58 @@
+##############################
+# Reading the files
+##############################
+
+citibike_jan19 <- read.csv(file = file.path('C:\\Users\\Yanik\\switchdrive\\SharedOstSchweiz',
+                                            '\\R Bootcamp\\Data\\201901-citibike-tripdata.csv'))
+
+citibike_apr19 <- read.csv(file = file.path('C:\\Users\\Yanik\\switchdrive\\SharedOstSchweiz',
+                                            '\\R Bootcamp\\Data\\201904-citibike-tripdata.csv'))
+
+citibike_jul19 <- read.csv(file = file.path('C:\\Users\\Yanik\\switchdrive\\SharedOstSchweiz',
+                                            '\\R Bootcamp\\Data\\201907-citibike-tripdata.csv'))
+
+citibike_oct19 <- read.csv(file = file.path('C:\\Users\\Yanik\\switchdrive\\SharedOstSchweiz',
+                                            '\\R Bootcamp\\Data\\201910-citibike-tripdata.csv'))
+
+nyc_weather_2019 <- read.csv(file = file.path('C:\\Users\\Yanik\\switchdrive\\SharedOstSchweiz',
+                                              '\\R Bootcamp\\Data\\New York Weather 2019.csv'))
+
+##############################
+# Sampleing the Dataframes
+##############################
+
+library(dplyr)
+
+citibike_jan19_sample <- sample_n(citibike_jan19, size=1000)
+citibike_apr19_sample <- sample_n(citibike_apr19, size=1000)
+citibike_jul19_sample <- sample_n(citibike_jul19, size=1000)
+citibike_oct19_sample <- sample_n(citibike_oct19, size=1000)
+
+##############################
+# Testing
+##############################
+
+head(citibike_jan19_sample)
+head(citibike_apr19_sample)
+head(citibike_jul19_sample)
+head(citibike_oct19_sample)
+
+
+##############################
+# Dataframe merging
+##############################
+
+
+citibike_merged <- rbind(citibike_jan19_sample, citibike_apr19_sample, 
+                         citibike_jul19_sample, citibike_oct19_sample)
+
+View(citibike_merged)
+nrow(citibike_merged)
+
+##############################
+# Write the csv
+##############################
+
+write.csv(citibike_merged, sep = ',', file = "citibike_merged_2019")
+
+
